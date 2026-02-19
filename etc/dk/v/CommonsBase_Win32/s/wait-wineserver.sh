2f70537b31462348bf8b0313fd19a18ce2493766
Jonah Beckford <9566106-jonahbeckford@users.noreply.gitlab.com>
2026-02-19 14:52:11 -0800

    CommonsBase_Win32.Wine@11.2.0 (2/n)
    + Add wait-wineserver.sh script

---
+++ etc/dk/v/CommonsBase_Win32/s/wait-wineserver.sh
@@ -0,0 +1,40 @@
+#!/bin/sh
+set -euf
+
+# Inspiration from:
+# https://github.com/mstorsjo/msvc-wine/blob/32b504c63b869681cda6824a20e30b74cb718432/Dockerfile#L8-L11
+
+## LICENSE.txt
+# The msvc-wine project - the scripts for downloading and setting up the
+# toolchain - is licensed under the ISC license.
+#
+# This license only covers the scripts themselves. In particular, it does
+# not conver the downloaded and installed tools.
+#
+# The ISC license:
+#
+# Permission to use, copy, modify, and/or distribute this software for any
+# purpose with or without fee is hereby granted, provided that the above
+# copyright notice and this permission notice appear in all copies.
+#
+# THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+# WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+# MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+# ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+# WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+# ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+# OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+##
+
+# Loop no more than 10 times
+i=0
+while pgrep -x wineserver && [ $i -lt 10 ]; do
+    sleep 1
+    i=$((i + 1))
+done
+
+# If exceeds 10 times then fail.
+if pgrep -x wineserver; then
+    echo 'FATAL: wineserver still running after 10 seconds'
+    exit 1
+fi
